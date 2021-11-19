0a2a870b42057ddff253c57cab6cb949f626065f
Floris de Vries <florisdev@hotmail.com>
2021-11-19 21:07:47 +0100

    MainMenu

---
+++ Assets/Scripts/ScriptableObjects/SceneManagerSO.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Scene
+{
+    MainMenu,
+    Tutorial,
+    Level1
+}
+
+[CreateAssetMenu(fileName = "SceneManager", menuName = "Managers/SceneManager")]
+public class SceneManagerSO : ScriptableObject
+{
+    public void ActivateScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void ActivateScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    internal void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/UI/StartSceneButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneButton : MonoBehaviour
+{
+    [SerializeField] private SceneManagerSO sceneManager;
+    [SerializeField] private bool _currentScene = false;
+    [SerializeField] private Scene _sceneToLoad = Scene.MainMenu;
+
+    private void OnEnable()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        if (_currentScene)
+            sceneManager.ReloadScene();
+        else
+            sceneManager.ActivateScene(_sceneToLoad);
+    }
+}
